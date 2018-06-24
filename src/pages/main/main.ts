7fdfb82387bcf7f07583da4e30cf1c5aa9dad4b9
Asmaa Hamdy <asmaa_hamdy8@hotmail.com>
2018-06-24 18:54:18 +0200

    Added main page and new measurements

--- src/pages/main/main.ts
+++ src/pages/main/main.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the RegisterPage page.
+ * Generated class for the MainPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,13 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-register',
-  templateUrl: 'register.html',
+  selector: 'page-main',
+  templateUrl: 'main.html',
 })
-export class RegisterPage {
+export class MainPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad MainPage');
+  }
+
 }
