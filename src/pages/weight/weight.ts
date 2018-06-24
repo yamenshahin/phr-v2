eb24192155b8ae7313df8d3d5e62a5048b19c019
Asmaa Hamdy <asmaa_hamdy8@hotmail.com>
2018-06-24 20:17:41 +0200

    Added quick access buttons on home

--- src/pages/weight/weight.ts
+++ src/pages/weight/weight.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 /**
- * Generated class for the RegisterPage page.
+ * Generated class for the WeightPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
@@ -10,16 +10,16 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @IonicPage()
 @Component({
-  selector: 'page-register',
-  templateUrl: 'register.html',
+  selector: 'page-weight',
+  templateUrl: 'weight.html',
 })
-export class RegisterPage {
+export class WeightPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  goTo(){
-    this.navCtrl.push('MeasurementPage');
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad WeightPage');
   }
-  
+
 }
