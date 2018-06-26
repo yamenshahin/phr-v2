cd5f9f3deb13dacfd6b162910263b3683e9966cc
Asmaa Hamdy <asmaa_hamdy8@hotmail.com>
2018-06-26 11:30:12 +0200

    generated chronic add page

---
+++ src/pages/add-chronic/add-chronic.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { IonicPageModule } from 'ionic-angular';
+import { AddChronicPage } from './add-chronic';
+
+@NgModule({
+  declarations: [
+    AddChronicPage,
+  ],
+  imports: [
+    IonicPageModule.forChild(AddChronicPage),
+  ],
+})
+export class AddChronicPageModule {}
---
+++ src/pages/add-chronic/add-chronic.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the AddChronicPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-add-chronic',
+  templateUrl: 'add-chronic.html',
+})
+export class AddChronicPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad AddChronicPage');
+  }
+
+}
