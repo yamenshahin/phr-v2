3c0c76952e92034ee46743768296c49e272aba46
Yamen Shahin <yamenshahin@gmail.com>
2018-06-30 23:45:39 +0200

    Remove 'prefix' = 'auth'

--- src/environment.ts
+++ src/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
 	'site_url' : 'http://127.0.0.1:8000/api/',
-	'login_url'  : 'auth/login',
+	'login_url'  : 'login',
 	'register_url'  : 'register',
 	'logout_url'   : 'logout?',
 	'recover_url'  : 'recover',
