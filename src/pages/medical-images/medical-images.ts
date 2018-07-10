22bb2d7f785fdba58ef87ff07b05317e767b8d3a
Asmaa Hamdy <asmaa_hamdy8@hotmail.com>
2018-07-10 12:16:48 +0200

    added get picture from gallery and crop image features BUT without testing

--- src/pages/medical-images/medical-images.ts
+++ src/pages/medical-images/medical-images.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
 import { Camera, CameraOptions } from '@ionic-native/camera';
-
 
 @IonicPage()
 @Component({
@@ -33,6 +31,43 @@ export class MedicalImagesPage {
     });
   }
 
+  getImage(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: true
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+    this.myphoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+  cropImage(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: this.camera.PictureSourceType.PHOTOLIBRARY,
+      saveToPhotoAlbum: true,
+      allowEdit: true,
+      targetWidth: 300,
+      targetHeight: 300
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+    this.myphoto = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
     
   /*  this.options = {
       quality: 100,
@@ -49,4 +84,6 @@ export class MedicalImagesPage {
       //Here is where errors happen.
     });
 }*/
+
+
 }
